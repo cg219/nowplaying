a9af1249521f5fb12eef1785856930b8d79de534
Mente Gee <dev@imkreative.com>
2024-12-31 01:02:39 -0500

    download db from r2 before startup and save a latest version along with timestamp

--- cmd/nowplaying/main.go
+++ cmd/nowplaying/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	_ "embed"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
@@ -20,6 +22,46 @@ import (
 func main() {
     cfg := app.NewConfig()
     done := make(chan struct{})
+    cwd, _ := os.Getwd();
+    s3cfg, err := config.LoadDefaultConfig(context.Background(), config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(os.Getenv("R2_KEY"), os.Getenv("R2_SECRET"), "")), config.WithRegion("auto"))
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    client := s3.NewFromConfig(s3cfg, func(o *s3.Options) {
+        o.BaseEndpoint = aws.String(os.Getenv("R2_URL"))
+    })
+
+    res, err := client.GetObject(context.Background(), &s3.GetObjectInput{
+        Bucket: aws.String("nowplaying"),
+        Key: aws.String("database.db"),
+    })
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    dbfile, err := os.Create(filepath.Join(cwd, os.Getenv("APP_DATA")))
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    data, err := io.ReadAll(res.Body)
+    if err != nil {
+        res.Body.Close()
+        log.Fatal(err)
+    }
+
+    res.Body.Close()
+
+    _, err = dbfile.Write(data)
+    if err != nil {
+        dbfile.Close()
+        log.Fatal(err)
+    }
+
+    dbfile.Close()
 
     go func() {
         if err := app.Run(*cfg); err != nil {
@@ -35,33 +77,42 @@ func main() {
     <- done
 
     if strings.EqualFold(os.Getenv("APP_EXIT_BACKUP"), "1") {
-        s3cfg, err := config.LoadDefaultConfig(context.Background(), config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(os.Getenv("R2_KEY"), os.Getenv("R2_SECRET"), "")), config.WithRegion("auto"))
-        if err != nil {
-            log.Fatal(err)
-        }
-
-        client := s3.NewFromConfig(s3cfg, func(o *s3.Options) {
-            o.BaseEndpoint = aws.String(os.Getenv("R2_URL"))
-        })
-
-        cwd, _ := os.Getwd();
         dbfile, err := os.Open(filepath.Join(cwd, os.Getenv("APP_DATA")))
+
         if err != nil {
             log.Fatal(err)
         }
+
+        var latest bytes.Buffer
+
+        tee := io.TeeReader(dbfile, &latest)
+        dbbuf := new(bytes.Buffer)
+        io.Copy(dbbuf, tee)
+        timestamped := bytes.NewReader(dbbuf.Bytes())
+        bckup := bytes.NewReader(latest.Bytes())
 
         timestamp := time.Now()
-        res, err := client.PutObject(context.Background(), &s3.PutObjectInput{
+        log.Println("Saving database to R2")
+
+        _, err = client.PutObject(context.Background(), &s3.PutObjectInput{
             Bucket: aws.String("nowplaying"),
             Key: aws.String(fmt.Sprintf("%d-database.db", timestamp.UnixMilli())),
-            Body: dbfile,
+            Body: timestamped,
         })
 
         if err != nil {
-            log.Fatal(err)
+            log.Fatal(err.Error())
         }
 
-        log.Println(res)
+        _, err = client.PutObject(context.Background(), &s3.PutObjectInput{
+            Bucket: aws.String("nowplaying"),
+            Key: aws.String("database.db"),
+            Body: bckup,
+        })
+
+        if err != nil {
+            log.Fatal(err.Error())
+        }
     }
 
     log.Println("Exiting nowplaying safely")
